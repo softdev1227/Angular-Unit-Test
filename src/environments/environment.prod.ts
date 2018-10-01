2def347f1ddc5ea48f81060263eaa8d369e49090
softdev1227 <soft_dev1227@outlook.com>
2018-10-01 14:21:59 +0300

    added hack for spa for gh pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDK_Wn9QRD2w6WHGf3x9z70WBb862IkzPY',
+    authDomain: 'galleryapp-dd4ef.firebaseapp.com',
+    databaseURL: 'https://galleryapp-dd4ef.firebaseio.com',
+    projectId: 'galleryapp-dd4ef',
+    storageBucket: 'galleryapp-dd4ef.appspot.com',
+    messagingSenderId: '303478029783'
+  }
 };
